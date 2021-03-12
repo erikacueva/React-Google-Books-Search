55dc33b72d121851a140456b607d7f39c1324a7c
erika cueva <ecueva88@gmail.com>
2021-03-11 23:14:27 -0800

    deploying

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooksearch",
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
